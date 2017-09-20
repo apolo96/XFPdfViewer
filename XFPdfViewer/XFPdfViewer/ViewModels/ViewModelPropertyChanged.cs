444612d2cc5d30435dcb835ecbc8397cd2ef0b10
apolo96 <andresfpvclap@gmail.com>
2017-09-20 10:51:29 -0500

    Add project files.

---
+++ XFPdfViewer/XFPdfViewer/ViewModels/ViewModelPropertyChanged.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+namespace XFPdfViewer.ViewModels
+{
+    public class ViewModelPropertyChanged : INotifyPropertyChanged
+    {
+        public void RaisePropertyChanged([CallerMemberName]string nameProperty = null)
+        {
+            var handle = PropertyChanged;
+            if (handle != null) handle(this, new PropertyChangedEventArgs(nameProperty));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
